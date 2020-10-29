fc06a78593f8708e8ce0b02044df0f37573c11b7
ariel_grubbs <arielvictor1234@gmail.com>
2020-10-29 15:41:03 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,27 @@
+require 'pry'
+
 class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+    @student = Student.all.find_by(id: params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
